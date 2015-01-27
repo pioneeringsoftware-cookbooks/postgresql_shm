e06e44a07e7ebb58bd50b15dd837f4e96420a137
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-01-27 17:28:57 +0000

    Set up SHMALL, SHMMAX and PostgreSQL shared buffers

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# See http://www.postgresql.org/message-id/attachment/2697/shmsetup
+page_size, _phys_pages = %w(PAGE_SIZE _PHYS_PAGES).map do |var|
+  `getconf #{var}`.chomp.to_i
+end
+
+shmall = (_phys_pages * node['postgresql']['shm']['all'].to_f).floor
+shmmax = shmall * page_size
+shared_buffers = (shmmax * node['postgresql']['shm']['buffers'].to_f).to_i
+
+node.default['sysctl']['params']['kernel']['shmall'] = shmall
+node.default['sysctl']['params']['kernel']['shmmax'] = shmmax
+
+node.default['postgresql']['config']['shared_buffers'] = "#{shared_buffers >> 10}kB"
