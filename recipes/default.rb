b3e4db487f586db4b2ee30a240265ef6750ba540
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-01-28 14:34:26 +0000

    Apache 2.0 license

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,25 @@
+#-------------------------------------------------------------------------------
 #
 # Cookbook Name:: postgresql_shm
-# Recipe:: default
+#        Recipe:: default
 #
-# Copyright (C) 2015 Roy Ratcliffe
+#-------------------------------------------------------------------------------
 #
-# All rights reserved - Do Not Redistribute
+# Copyright (c) 2015, Pioneering Software, United Kingdom. All rights reserved.
 #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+#-------------------------------------------------------------------------------
 
 # See http://www.postgresql.org/message-id/attachment/2697/shmsetup
 page_size, _phys_pages = %w(PAGE_SIZE _PHYS_PAGES).map do |var|
