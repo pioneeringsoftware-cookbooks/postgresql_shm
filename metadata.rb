7fc46bf84810503ffc98bee1587114e1734b4b98
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-07-08 13:54:53 +0100

    Long description comes from README.md (version 0.2.0)

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer       'Roy Ratcliffe'
 maintainer_email 'roy@pioneeringsoftware.co.uk'
 license          'All rights reserved'
 description      'Installs/Configures postgresql_shm'
-long_description 'Installs/Configures postgresql_shm'
-version          '0.1.2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
